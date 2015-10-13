0472a87d278fc44d87ab5b876c5791a709c9b4dc
Vitras <k.kuijer@students.uu.nl>
2015-10-13 16:33:33 +0200

    First push (server en client prototypes)

---
+++ Prototype 1.0/Client/Assets/ColorSwitch.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorSwitch : MonoBehaviour
+{
+	Material mat;
+	public int setter;
+	// Use this for initialization
+	void Start ()
+	{
+		mat = this.GetComponent<Renderer> ().material;
+		setter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		switch (setter) {
+		case 0:
+			mat.color = Color.white;
+			break;			
+		case 1:
+			mat.color = Color.red;
+			break;			
+		case 2:
+			mat.color = Color.blue;
+			break;			
+		case 3:
+			mat.color = Color.black;
+			break;
+
+		}
+	}
+}
