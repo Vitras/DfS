60c5df745b7fd7628b0d48e07d4b3f2954ac9615
Kylian Kuijer <k.kuijer@students.uu.nl>
2015-11-30 15:58:47 +0100

    Triggers vullen zichzelf.

--- Prototype 1.0/Client/Assets/Server/Scripts/EnvironmentTriggers.cs
+++ Prototype 1.0/Client/Assets/Server/Scripts/EnvironmentTriggers.cs
@@ -8,6 +8,7 @@ public class EnvironmentTriggers : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
+		SortGameObjects ();
 	}
 	
 	// Update is called once per frame
@@ -15,5 +16,36 @@ public class EnvironmentTriggers : MonoBehaviour
 	{
 		TriggerScript trigger = networkTriggers [index].GetComponent<TriggerScript> ();
 		trigger.Triggered = !trigger.Triggered;
+	}
+
+	public void SortGameObjects()
+	{
+		int children = transform.childCount;
+		for(int x = 0; x < children; x++)
+		{
+			string name = transform.GetChild (x).name;
+			if(!name.Contains("|"))
+				continue;
+			string[] line = name.Split('|');
+			int suffixValue = 0;
+			switch (line [2]) {
+			case "left":
+				break;
+			case "right":
+				suffixValue = 1;
+				break;
+			case "top":
+				suffixValue = 2;
+				break;
+			case "bottom":
+				suffixValue = 3;
+				break;
+			default:
+				break;
+			}
+			int triggerID = (int.Parse (line [1]) - 1) * 4 + suffixValue;
+			Debug.Log (triggerID);
+			networkTriggers[triggerID] = transform.GetChild (x).gameObject;
+		}
 	}
 }
