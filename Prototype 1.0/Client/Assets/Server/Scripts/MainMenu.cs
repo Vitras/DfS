2d24cd52a73dbcfb27f16c6ae40d348ac9bbdb2b
Vitras <k.kuijer@students.uu.nl>
2015-12-10 22:45:59 +0100

    Menuing tussencommit

--- Prototype 1.0/Client/Assets/Server/Scripts/MainMenu.cs
+++ Prototype 1.0/Client/Assets/Server/Scripts/MainMenu.cs
@@ -4,61 +4,78 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour
+{
 
-	private enum MenuState{None, Button, Options};
+	private enum MenuState
+	{
+		None,
+		Button,
+		Options}
+	;
 	private MenuState state;
 	private float startTimer;
+	public bool fadeIn;
+	private float fadeSpeed;
 	public GameObject optionsArray;
 	public GameObject buttonIndicator;
 	// Use this for initialization
 
-	void Start () {
+	void Start ()
+	{
 		state = MenuState.None;
-		startTimer = 3;
+		fadeSpeed = 0.8f;
+		startTimer = 1.5f;		
+		optionsArray.GetComponent<CanvasGroup> ().alpha = 0;
 		optionsArray.SetActive (false);
 		buttonIndicator.SetActive (false);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if(state == MenuState.None)
-		{
+	void Update ()
+	{
+		if (state == MenuState.None) {
 			startTimer -= Time.deltaTime;
-			if(startTimer < 0)
-			{
-				state = MenuState.Button;
-				buttonIndicator.SetActive (true);
+			if (startTimer < 0) {
+				state = MenuState.Options;
+				//buttonIndicator.SetActive (true);
+				fadeIn = true;
+				optionsArray.GetComponent<CanvasGroup> ().alpha = 0;
+				optionsArray.SetActive (true);				
+				EventSystem.current.SetSelectedGameObject (optionsArray.transform.GetChild (0).gameObject);
 			}
-		}
-		else if(state == MenuState.Button)
-		{
-			if(Input.anyKeyDown)
-			{
+		} else if (state == MenuState.Button) {
+			if (Input.anyKeyDown) {
 				state = MenuState.Options;
 				optionsArray.SetActive (true);
-				buttonIndicator.SetActive(false);
-				EventSystem.current.SetSelectedGameObject(optionsArray.transform.GetChild(0).gameObject);
+				buttonIndicator.SetActive (false);
+				EventSystem.current.SetSelectedGameObject (optionsArray.transform.GetChild (0).gameObject);
 			}
-		}
-		else
-		{
-			if(Input.GetKeyDown(KeyCode.JoystickButton3) || Input.GetKeyDown (KeyCode.Backspace))
-			{
-				state = MenuState.Button;
-				optionsArray.SetActive (false);				
-				buttonIndicator.SetActive(true);
+		} else {
+			if (fadeIn) {					
+				optionsArray.GetComponent<CanvasGroup> ().alpha += fadeSpeed * Time.deltaTime;
+				fadeIn = !(optionsArray.GetComponent<CanvasGroup> ().alpha == 1);
+				
+			} else {					
+				optionsArray.GetComponent<CanvasGroup> ().alpha -= fadeSpeed * Time.deltaTime;
+				fadeIn = optionsArray.GetComponent<CanvasGroup> ().alpha == 0;
 			}
+//			if(Input.GetKeyDown(KeyCode.JoystickButton3) || Input.GetKeyDown (KeyCode.Backspace))
+//			{
+//				state = MenuState.Button;
+//				optionsArray.SetActive (false);				
+//				buttonIndicator.SetActive(true);
+//			}
 		}
 	}	
 	
-	public void GoToOptions()
+	public void GoToOptions ()
 	{
-		Application.LoadLevel("Options");
+		Application.LoadLevel ("Options");
 	}	
 	
-	public void StartGame()
+	public void StartGame ()
 	{
-		Application.LoadLevel("GameSettings");
+		Application.LoadLevel ("GameSettings");
 	}
 }
